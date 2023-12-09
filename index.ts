6a12ff364ab1336724b5f2da98ff4ec137f3f0b3
tai03102004 <124711101+tai03102004@users.noreply.github.com>
2023-12-09 19:23:53 +0700

    update

--- index.ts
+++ index.ts
@@ -1,14 +1,29 @@
 import express, {Express,Request,Response} from 'express';
+import dotenv from 'dotenv';
+
+import * as database from "./config/database";
+
+import Article from "./models/article.model";
+
+dotenv.config();
+
+database.connect();
 
 const app : Express = express();
-const port: number = 3000;
+const port: number | string = process.env.port || 3000;
 
 // Rest api
 
-app.get("/articles" , (req: Request, res: Response)  => {
+app.get("/articles" , async (req: Request, res: Response)  => {
+
+    const articles = await Article.find ({
+        deleted : false,
+    });
+
     res.json ({
         articles: []
-    })
+    });
+
 });
 
 app.listen (port ,()  => {
