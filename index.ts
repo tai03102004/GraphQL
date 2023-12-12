dbd68ae7a85151d73389bf6992e2e626ecfeefce
tai03102004 <124711101+tai03102004@users.noreply.github.com>
2023-12-12 19:30:00 +0700

    update

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-import express, {Express,Request,Response} from 'express';
+import express, {Express} from 'express';
 import dotenv from 'dotenv';
 
 import * as database from "./config/database";
 
-import {ApolloServer,gql} from "apollo-server-express";
+import {ApolloServer} from "apollo-server-express";
 
 import {typeDefs} from "./typeDefs/index.typeDefs";
 
